1e575197e25279db5c213fa08618ba8830d956ab
AslamLert <gloommyday@gmail.com>
2021-08-31 21:22:28 +0700

    Get all employees

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function getEmployee(){
+        return response()->json(Employee::all(), 200);
+    }
 }
